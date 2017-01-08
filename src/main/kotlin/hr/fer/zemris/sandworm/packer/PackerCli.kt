0aa71a4cc0bb8c4a69e80b868d1d123496a8e3bd
Petar Šegina <psegina@ymail.com>
2017-01-08 22:41:55 +0100

    Convert to command-line application

--- src/main/kotlin/hr/fer/zemris/sandworm/packer/PackerCli.kt
+++ src/main/kotlin/hr/fer/zemris/sandworm/packer/PackerCli.kt
@@ -1,13 +1,31 @@
 package hr.fer.zemris.sandworm.packer
 
+import org.apache.commons.cli.DefaultParser
+import org.apache.commons.cli.HelpFormatter
+import org.apache.commons.cli.Options
 import java.io.File
+
 
 fun main(args: Array<String>) {
 
+    val options = Options().apply {
+        addOption("b", "base-image", true, "Define the base image to build upon (e.g. sandworm/base/c_cpp")
+        addOption("s", "source-directory", true, "The location from which to take the source and the input files")
+    }
+
+    val parser = DefaultParser()
+
+    val commandLine = parser.parse(options, args)
+
+    if (!commandLine.hasOption('b') || !commandLine.hasOption('s')) {
+        HelpFormatter().printHelp("packer", options)
+        return
+    }
+
     Packer.pack(
             File("src/main/resources/c_sample"),
-            "sandworm/base/c_cpp",
-            "sandworm/images/"
+            commandLine.getOptionValue('b'),
+            commandLine.getOptionValue('s')
     )
 
 }
